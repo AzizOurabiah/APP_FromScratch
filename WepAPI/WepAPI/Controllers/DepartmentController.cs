e0cfcc831e2fe7ce69d624ee26b40d47ec591744
Aziz-Ourabiah-Hotmail <ourabiahaziz1@gmail.com>
2023-01-02 13:34:11 -0500

    Implémentation de tous les méthodes Get Post, Update et Delet dans le controller Department

--- WepAPI/WepAPI/Controllers/DepartmentController.cs
+++ WepAPI/WepAPI/Controllers/DepartmentController.cs
@@ -64,6 +64,47 @@ namespace WepAPI.Controllers
             return new JsonResult(table);
         }
 
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            //On crée la requête 
+            string query = @"
+                insert into Department values ('"+ dep.DepartmentName +"')";
+
+            //Création de la table pour les données
+            DataTable table = new DataTable();
+
+            //Création la variable de connection
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+            //Création la variable pour lire les données
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                //Ouverture de la connection
+                myCon.Open();
+
+                //Utilisation de SqlCommande avec la requête et la chaine de connection
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    //Exécuter la commande 'myCommand' et mettre les résulats dans l'objet myReader
+                    myReader = myCommand.ExecuteReader();
+
+                    //Mettre les données dans la table
+                    table.Load(myReader);
+
+                    //Ferméture l'objet de lecture
+                    myReader.Close();
+
+                    //Fermeture la chaine de connection
+                    myCon.Close();
+                }
+            }
+            //On retourne la table des données sous format de JSON
+            return new JsonResult("Added successfully ");
+        }
+
         [HttpPut]
         public JsonResult Put(Department dep)
         {
@@ -102,6 +143,45 @@ namespace WepAPI.Controllers
             }
             //On retourne la table des données sous format de JSON
             return new JsonResult("Update Successfully !");
+        }
+        
+        [HttpDelete("{id}")]
+        public JsonResult Delate(int id) 
+        {
+            //On crée la requête 
+            string query = @"
+                delete from dbo.Department             
+                where DepartmentId = " + id + "";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                //Ouverture de la connection
+                myCon.Open();
+
+                //Utilisation de SqlCommande avec la requête et la chaine de connection
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    //Exécuter la commande 'myCommand' et mettre les résulats dans l'objet myReader
+                    myReader = myCommand.ExecuteReader();
+
+                    //Mettre les données dans la table
+                    table.Load(myReader);
+
+                    //Ferméture l'objet de lecture
+                    myReader.Close();
+
+                    //Fermeture la chaine de connection
+                    myCon.Close();
+                }
+            }
+            //On retourne la table des données sous format de JSON
+            return new JsonResult("Deleted Successfully !");
         }
     }
 }
