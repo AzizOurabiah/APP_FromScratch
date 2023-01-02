ad9df27c19e2d950d19103b4eec31788b211d712
Aziz-Ourabiah-Hotmail <ourabiahaziz1@gmail.com>
2023-01-02 10:08:46 -0500

    Scripte de création de la base de données et des tables

---
+++ Cration_des_tables_et_base_de_données.sql
@@ -0,0 +1,31 @@
+--Création de database
+create database EmployeeDB2
+go
+
+use EmployeeDB2
+go
+
+--création des tables
+create table dbo.Department(
+DepartmentId int identity(1,1),
+DepartmentName varchar(500)
+)
+
+select * from dbo.Department
+
+insert into dbo.Department values ('Support')
+insert into dbo.Department values ('IT')
+
+
+create table dbo.Employee(
+EmployeeId int identity(1,1),
+DepartmentName varchar(500),
+Department varchar(500),
+DateOfJoining date,
+PhotoFileName varchar(500)
+)
+insert into dbo.Employee values('Sam', 'It', '2020-06-01', 'anonymous.png')
+
+select * from Employee
+
+
