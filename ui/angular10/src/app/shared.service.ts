66989ca2a73127b7a412642ffc0efda7af7db4fc
Aziz-Ourabiah-Hotmail <ourabiahaziz1@gmail.com>
2023-01-03 12:00:14 -0500

    Ajout tous les méthodes get post put and delete dans le controller Emplyee et l'ajout de uploadPhoto et getAllDepartmentName

--- ui/angular10/src/app/shared.service.ts
+++ ui/angular10/src/app/shared.service.ts
@@ -12,23 +12,58 @@ export class SharedService {
 
   constructor(private http: HttpClient) {}
 
+  /**************** Méthode qui concerne Départment controller *************** */
   //Création une méthode pour avoir les données
   getDepList(): Observable<any[]> {
-    return this.http.get<any>(this.APIUrl + '/department');
+    return this.http.get<any>(this.APIUrl + '/Department');
   }
 
   //Méthode pour ajouter un department (Post)
   addDepartment(val: any) {
-    return this.http.post(this.APIUrl + '/department', val);
+    return this.http.post(this.APIUrl + '/Department', val);
   }
 
   //Méthode for updated
   updateDepartment(val: any) {
-    return this.http.put(this.APIUrl + '/department', val);
+    return this.http.put(this.APIUrl + '/Department', val);
   }
 
   //Méthode pour supprimer department
   deleteDepartment(val: any) {
-    return this.http.delete(this.APIUrl + '/department', val);
+    return this.http.delete(this.APIUrl + '/Department', val);
+  }
+
+  /**************** Méthode qui concerne Employee controller *************** */
+
+  //Création une méthode pour avoir les données
+  getEmpList(): Observable<any[]> {
+    return this.http.get<any>(this.APIUrl + '/Employee');
+  }
+
+  //Méthode pour ajouter un employee (Post)
+  addEmployee(val: any) {
+    return this.http.post(this.APIUrl + '/Employee', val);
+  }
+
+  //Méthode for updated
+  updateEmployee(val: any) {
+    return this.http.put(this.APIUrl + '/Employee', val);
+  }
+
+  //Méthode pour supprimer employee
+  deleteEmployee(val: any) {
+    return this.http.delete(this.APIUrl + '/Employee', val);
+  }
+
+  //Méthode pour uploader photo
+  uploadPhoto(val: any) {
+    return this.http.post(this.APIUrl + 'Employee/SaveFile', val);
+  }
+
+  //Méthode pour avoir tout les departement
+  getAllDepartmentName(): Observable<any[]> {
+    return this.http.get<any[]>(
+      this.APIUrl + '/Employee/GetAllDepartmentNames'
+    );
   }
 }
