3f4e1e92d3520862589d6b177a4051d94c70748d
Aziz-Ourabiah-Hotmail <ourabiahaziz1@gmail.com>
2023-01-03 10:46:28 -0500

    Implémentation de tous les méthode get post put et delete qui concerne Department dans le fichier sharedService

--- ui/angular10/src/app/shared.service.ts
+++ ui/angular10/src/app/shared.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SharedService {
+  //On creé les adresse de l'API et de la Photo
+  readonly APIUrl = 'http:localhost:47357/api';
+  readonly PhotoUrl = 'localhost:47357/Photos';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  //Création une méthode pour avoir les données
+  getDepList(): Observable<any[]> {
+    return this.http.get<any>(this.APIUrl + '/department');
+  }
+
+  //Méthode pour ajouter un department (Post)
+  addDepartment(val: any) {
+    return this.http.post(this.APIUrl + '/department', val);
+  }
+
+  //Méthode for updated
+  updateDepartment(val: any) {
+    return this.http.put(this.APIUrl + '/department', val);
+  }
+
+  //Méthode pour supprimer department
+  deleteDepartment(val: any) {
+    return this.http.delete(this.APIUrl + '/department', val);
+  }
 }
