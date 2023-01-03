b67d388305d36b09f7d81bb8e60886100f189813
Aziz-Ourabiah-Hotmail <ourabiahaziz1@gmail.com>
2023-01-03 12:14:38 -0500

    Création routing

--- ui/angular10/src/app/app-routing.module.ts
+++ ui/angular10/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { DepartmentComponent } from './department/department.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'employee',
+    component: EmployeeComponent,
+  },
+  {
+    path: 'department',
+    component: DepartmentComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
