4e19f6c947a0f77f85f641a6524e3eeb60391362
Aziz-Ourabiah-Hotmail <ourabiahaziz1@gmail.com>
2023-01-02 21:14:44 -0500

    Ajout de la méthode GetAllDepartmentName

--- WepAPI/WepAPI/Controllers/EmployeeController.cs
+++ WepAPI/WepAPI/Controllers/EmployeeController.cs
@@ -191,7 +191,7 @@ namespace WepAPI.Controllers
             //On retourne la table des données sous format de JSON
             return new JsonResult("Deleted Successfully !");
         }
-
+        //Méthode pour sauvegarder les photos
         [Route("SaveFile")]
         [HttpPost]
         public JsonResult SaveFile()
@@ -213,6 +213,47 @@ namespace WepAPI.Controllers
             {
                 return new JsonResult("anonymous.png");
             }
+        }
+        [Route("GetAllDepartmentName")]
+        [HttpGet]
+        public JsonResult GetAllDepartmentName()
+        {
+            //On crée la requête 
+            string query = @"
+                select DepartmentName from dbo.Department";
+
+            //Création de la table pour les données
+            DataTable table = new DataTable();
+
+            //Création la variable de connection
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+            //Création la variable pour lire les données
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                //Ouverture de la connection
+                myCon.Open();
+
+                //Utilisation de SqlCommande avec la requête et la chaine de connection
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    //Exécuter la commande 'myCommand' et mettre les résulats dans l'objet myReader
+                    myReader = myCommand.ExecuteReader();
+
+                    //Mettre les données dans la table
+                    table.Load(myReader);
+
+                    //Ferméture l'objet de lecture
+                    myReader.Close();
+
+                    //Fermeture la chaine de connection
+                    myCon.Close();
+                }
+            }
+            //On retourne la table des données sous format de JSON
+            return new JsonResult(table);
         }
     }
 }
