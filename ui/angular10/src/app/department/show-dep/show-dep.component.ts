90480cd3fee9b30a83ec811e208f17037e49f71b
Aziz-Ourabiah-Hotmail <ourabiahaziz1@gmail.com>
2023-01-03 19:56:37 -0500

    Implémetation des boutoun Department et EMployeee et la table pour department et faire la connection pour le bouton Department

--- ui/angular10/src/app/department/show-dep/show-dep.component.ts
+++ ui/angular10/src/app/department/show-dep/show-dep.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-dep',
   templateUrl: './show-dep.component.html',
-  styleUrls: ['./show-dep.component.css']
+  styleUrls: ['./show-dep.component.css'],
 })
 export class ShowDepComponent implements OnInit {
-
-  constructor() { }
+  DepartmentLsit: any[] | undefined;
+  constructor(private service: SharedService) {}
 
   ngOnInit(): void {
+    //Lors de l'initialisation on va récupérer les données
+    this.refreshDeptList();
   }
 
+  //On crée une méthode qui récupère les données de shared service
+  refreshDeptList() {
+    this.service.getDepList().subscribe((data) => {
+      this.DepartmentLsit = data;
+    });
+  }
 }
