ec668f0a6b1733766d8007364617682e39496519
Aziz-Ourabiah-Hotmail <ourabiahaziz1@gmail.com>
2023-01-03 20:11:53 -0500

    Suite pour la logique du bouton Employee

--- ui/angular10/src/app/employee/shoe-emp/shoe-emp.component.ts
+++ ui/angular10/src/app/employee/shoe-emp/shoe-emp.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-shoe-emp',
   templateUrl: './shoe-emp.component.html',
-  styleUrls: ['./shoe-emp.component.css']
+  styleUrls: ['./shoe-emp.component.css'],
 })
 export class ShoeEmpComponent implements OnInit {
+  EmployeeList: any[] | undefined;
 
-  constructor() { }
+  constructor(private service: SharedService) {}
 
   ngOnInit(): void {
+    //initialisé empList dès l'initialisation
+    this.refreshData();
   }
 
+  //On crée une méthode pour checher les données
+  refreshData() {
+    this.service.getEmpList().subscribe((data) => {
+      this.EmployeeList = data;
+    });
+  }
 }
